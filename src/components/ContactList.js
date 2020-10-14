50ab6ecadaaabf4ab70e5dc8cff56e57139cd540
flavienbesseau <64224833+flavienbesseau@users.noreply.github.com>
2020-10-14 14:32:33 +0200

    initial commit

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,54 +1,45 @@
-import React from 'react';
+import React from "react";
+import Contact from "./Contact";
 
 const users = [
   {
-    name: 'Robert Reyes',
-    avatar: 'https://randomuser.me/api/portraits/men/28.jpg',
-    online: false,
+    name: "Robert Reyes",
+    avatar: "https://randomuser.me/api/portraits/men/28.jpg",
+    online: false
   },
   {
-    name: 'Nellie Caldwell',
-    avatar: 'https://randomuser.me/api/portraits/women/17.jpg',
-    online: true,
+    name: "Nellie Caldwell",
+    avatar: "https://randomuser.me/api/portraits/women/17.jpg",
+    online: true
   },
   {
-    name: 'Vernon Mason',
-    avatar: 'https://randomuser.me/api/portraits/men/84.jpg',
-    online: true,
+    name: "Vernon Mason",
+    avatar: "https://randomuser.me/api/portraits/men/84.jpg",
+    online: true
   },
   {
-    name: 'Erica Hunt',
-    avatar: 'https://randomuser.me/api/portraits/women/87.jpg',
-    online: false,
+    name: "Erica Hunt",
+    avatar: "https://randomuser.me/api/portraits/women/87.jpg",
+    online: false
   },
   {
-    name: 'Juanita Phillips',
-    avatar: 'https://randomuser.me/api/portraits/women/55.jpg',
-    online: true,
-  },
+    name: "Juanita Phillips",
+    avatar: "https://randomuser.me/api/portraits/women/55.jpg",
+    online: true
+  }
 ];
 
-const ContactList = () => {
-  return (
-    <div>
-      {users.map((member) => (
-        <div key={member} className="Contact">
-          <img src={member.avatar} alt={member.name} className="avatar" />
-          <div>
-            <p className="name">{member.name}</p>
-            <div className="status">
-              <i
-                className={member.online ? 'status-online' : 'status-offline'}
-              ></i>
-              <p className="status-text">
-                {member.online ? 'Online' : 'Offline'}
-              </p>
-            </div>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
-};
+const ContactList = () => (
+  <div>
+    {users.map((person) => (
+      <Contact
+        key={person.name}
+        name={person.name}
+        avatar={person.avatar}
+        online={person.online}
+      />
+    ))}
+  </div>
+);
 
 export default ContactList;
